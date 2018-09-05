3b725bbce7919b160b9906644a67dfb7560dd11a
Nathan Hunt <neighthan.hunt@gmail.com>
2018-09-04 22:39:13 -0400

    Basic endpoints, README documentation.

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # comet_ml_api
-Python bindings for the CometML REST API.
+
+This is a set of _unofficial_ Python bindings for the CometML REST API. There are functions for all current endpoints as well as a couple of functions that build on the basic endpoint ones to provide, e.g., simpler output or filtering. I haven't used all of the endpoints myself, so some of the functions haven't been tested at all (e.g. `get_html`). Documentation is currently limited but the functions provided are generally quite simple.
+
+See [Endpoints][endpoints] for the official documentation of the REST API.
+
+## Authentication
+
+See [this page][api key] first for how to generate your REST API key. There are two possible methods of authentication using that key:
+1. Store the key in `~/.comet_rest_key`. When you import the `api` script, it will automatically load a key that it finds in this file.
+2. Call the `set_api_key` function and pass your key in. The key will be saved in a global variable so that the same key is used for all subsequent requests unless you explicitly set a new key.
+
+## Endpoints
+
+Most of the basic endpoint functions are named `get_<endpoint>` where `<endpoint>` is the corresponding endpoint (e.g. `get_workspaces` to access `https://www.comet.ml/api/rest/v1/workspaces`). However, there are a few exceptions.
+
+### Metrics
+There are two endpoints for metrics: `metrics` and `metrics-raw`. I call both of these "raw" because they return data that isn't well-formatted for immediate plotting/analysis. As the `metrics` endpoint only return the min, max, and most recent data points for a given metric, I call that one a summary, hence `get_raw_metric_summaries`; the function for `metrics-raw` is `get_raw_metrics`. There is also a helper function `get_metrics` which converts the metrics into a better format for visualization or analysis.
+
+### Params
+Similarly to metrics, the raw parameters data may not be in the most usable format right away. I thus also call this endpoint `get_raw_params` and have a helper function `get_params` which provides a more concise output.
+
+### Other
+As for params except, though the endpoint is `log-other`, the functions are `get_raw_others` and `get_others`.
+
+### Images
+The `images` endpoint doesn't return the images themselves, just the data about them (including the URLs from which the actual images can be downloaded?). I call this endpoint `get_image_data`, but I haven't tested it.
+
+## Example Usage
+
+```python
+from comet_ml_api import api
+
+workspaces = api.get_workspaces()
+project_ids = api.get_project_names_and_ids(workspaces[0]) # {name: id}
+experiments = api.get_experiments(project_ids.popitem()[1])
+api.get_params(experiments[0]["experiment_key"])
+```
+
+[endpoints]: https://www.comet.ml/docs/rest-api/endpoints/
+[api key]: https://www.comet.ml/docs/rest-api/getting-started/
---
+++ api.py
@@ -0,0 +1,178 @@
+from pathlib import Path
+from typing import Optional, Dict, List, Any
+import json
+import requests
+import pandas as pd
+
+try:
+    with open(f"{Path.home()}/.comet_rest_key") as f:
+        API_KEY = f.read().strip()
+except (FileNotFoundError, PermissionError):
+    API_KEY = ""
+
+
+def set_api_key(key: str) -> None:
+    global API_KEY
+    API_KEY = key
+
+
+def _get_url(url: str, url_params: Optional[Dict[str, str]]=None) -> dict:
+    response = requests.get(url, url_params, headers={"Authorization": API_KEY})
+
+    if response.ok:
+        return json.loads(response.content)
+    else:
+        response.raise_for_status()
+
+
+def get_workspaces() -> List[str]:
+    url = "https://www.comet.ml/api/rest/v1/workspaces"
+    return _get_url(url)["workspaces"]
+
+
+def get_projects(workspace: str) -> List[Dict[str, Any]]:
+    url = f"https://www.comet.ml/api/rest/v1/projects"
+    url_params = {"workspace": workspace}
+    return _get_url(url, url_params)["projects"]
+
+
+def get_project_names(workspace: str) -> List[str]:
+    projects = get_projects(workspace)
+    return [project["project_name"] for project in projects]
+
+
+def get_project_names_and_ids(workspace: str) -> List[str]:
+    projects = get_projects(workspace)
+    return {project["project_name"]: project["project_id"] for project in projects}
+
+
+def get_experiments(project_id: str) -> List[Dict[str, Any]]:
+    url = f"https://www.comet.ml/api/rest/v1/experiments"
+    url_params = {"projectId": project_id}
+    return _get_url(url, url_params)["experiments"]
+
+
+def get_raw_params(experiment_key: str) -> List[Dict[str, Any]]:
+    url = f"https://www.comet.ml/api/rest/v1/experiment/params"
+    url_params = {"experimentKey": experiment_key}
+    params = _get_url(url, url_params)["results"]
+    return params
+
+
+def get_params(experiment_key: str) -> Dict[str, Any]:
+    raw_params = get_raw_params(experiment_key)
+    return {param["name"]: param["valueCurrent"] for param in raw_params}
+
+
+def get_raw_others(experiment_key: str) -> List[Dict[str, Any]]:
+    url = f"https://www.comet.ml/api/rest/v1/experiment/log-other"
+    url_params = {"experimentKey": experiment_key}
+    others = _get_url(url, url_params)["logOtherList"]
+    return others
+
+
+def get_others(experiment_key: str) -> Dict[str, Any]:
+    raw_others = get_raw_others(experiment_key)
+    return {other["name"]: other["valueCurrent"] for other in raw_others}
+
+
+def get_html(experiment_key: str) -> str:
+    url = f"https://www.comet.ml/api/rest/v1/experiment/html"
+    url_params = {"experimentKey": experiment_key}
+    return _get_url(url, url_params)["html"]
+
+
+def get_code(experiment_key: str) -> str:
+    url = f"https://www.comet.ml/api/rest/v1/experiment/code"
+    url_params = {"experimentKey": experiment_key}
+    return _get_url(url, url_params)["code"]
+
+
+def get_stdout(experiment_key: str) -> str:
+    url = f"https://www.comet.ml/api/rest/v1/experiment/stdout"
+    url_params = {"experimentKey": experiment_key}
+    return _get_url(url, url_params)["output"]
+
+
+def get_installed_packages(experiment_key: str) -> List[str]:
+    url = f"https://www.comet.ml/api/rest/v1/experiment/installed-packages"
+    url_params = {"experimentKey": experiment_key}
+    return _get_url(url, url_params)["packages"]
+
+
+def get_graph(experiment_key: str) -> str:
+    """
+    No idea what format the graph is in...
+    """
+    url = f"https://www.comet.ml/api/rest/v1/experiment/graph"
+    url_params = {"experimentKey": experiment_key}
+    return _get_url(url, url_params)["graph"]
+
+
+def get_image_data(experiment_key: str) -> List[Dict[str, Any]]:
+    url = f"https://www.comet.ml/api/rest/v1/experiment/images"
+    url_params = {"experimentKey": experiment_key}
+    return _get_url(url, url_params)["images"]
+
+
+def get_raw_metrics(experiment_key: str):
+    url = f"https://www.comet.ml/api/rest/v1/experiment/metrics-raw"
+    url_params = {"experimentKey": experiment_key}
+    return _get_url(url, url_params)["metrics"]
+
+
+def get_raw_metric_summaries(experiment_key: str) -> List[Dict[str, Any]]:
+    url = f"https://www.comet.ml/api/rest/v1/experiment/metrics"
+    url_params = {"experimentKey": experiment_key}
+    return _get_url(url, url_params)["results"]
+
+
+def get_metrics(experiment_key: str) -> Dict[str, pd.DataFrame]:
+    # TODO: are the metric values really always strings?
+    # I assume that all metric values are able to be converted to floats
+
+    # for each metric, return a dataframe so that the user can have plots
+    # indexed by timestamp, step, or timestamp + offset
+    # (I assume that )
+    # I don't know what "runContext" is, but I ignore it
+
+    raw_metrics = get_raw_metrics(experiment_key)
+    metrics = {}  # {metric_name: [(value, step, timestamp, offset)]}
+
+    for raw_metric in raw_metrics:
+        name = raw_metric["metricName"]
+        raw_metric = (float(raw_metric["metricValue"]), raw_metric["step"], raw_metric["timestamp"], raw_metric["offset"])
+
+        if name in metrics:
+            metrics[name].append(raw_metric)
+        else:
+            metrics[name] = [raw_metric]
+
+    metrics = {name: pd.DataFrame(metrics[name], columns=["value", "step", "timestamp", "offset"]) for name in metrics}
+    return metrics
+
+
+def get_experiments_with_params(project_id: str, query_params: Dict[str, Any]):
+    """
+    Return the keys of the experiments in `project_id` that match all of the query params.
+
+    There are valueMin, valueMax, and valueCurrent for each param, but I think these should
+    all be the same? I just check valueCurrent. I also convert to strings when checking
+    equality (so querying for 200 is the same as for "200") because it seems that
+    strings are always used in Comet's stored parameters.
+    """
+
+    experiments = get_experiments(project_id)
+    experiment_keys = []
+    for experiment in experiments:
+        add_experiment = True
+        key = experiment["experiment_key"]
+
+        params = get_params(key)
+        for name, value in query_params.items():
+            matched = name in params and params[name] == value
+            add_experiment &= matched
+
+        if add_experiment:
+            experiment_keys.append(key)
+    return experiment_keys
